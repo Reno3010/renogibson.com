596445e89011aef5fb67b3eb2ce8d0a90d422147
Reno Gibson <reno3010@gmail.com>
2011-09-25 14:01:04 -0400

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RenoCms::Application.config.secret_token = '1af12800d403731bc14e38889384c507c4109f431b6a75142f93cfc8788e46119b88c36bc7ac2b27337b8b6d797b72bb0937987fb1432e3494936e212f2bec35'
